c95de569df155bfb98d9e56bc6f1b48828d5a38d
Andra Antariksa <andra.antariksa@gmail.com>
2024-12-12 01:58:03 +0700

    feat: install clipboard library

--- src/ui/screens/details/components/TransactionId.tsx
+++ src/ui/screens/details/components/TransactionId.tsx
@@ -1,11 +1,16 @@
 import { TouchableOpacity, StyleSheet, Text } from "react-native";
+import * as Clipboard from "expo-clipboard";
 
 export type TransactionIdProps = {
     transactionId: string;
 };
 
 function TransactionId({ transactionId }: TransactionIdProps) {
-    return <TouchableOpacity style={style.containerHeader}>
+    const copyText = () => {
+        Clipboard.setStringAsync(transactionId);
+    };
+
+    return <TouchableOpacity style={style.containerHeader} onPress={copyText}>
         <Text style={style.textTitle}>ID TRANSAKSI: {transactionId}</Text>
     </TouchableOpacity>
 };
