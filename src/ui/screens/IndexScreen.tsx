606a1a813f667063428a4d18c34e50c2d5e04a71
Andra Antariksa <andra.antariksa@gmail.com>
2024-12-11 22:03:06 +0700

    feat: add padding to transaction index

--- src/ui/screens/IndexScreen.tsx
+++ src/ui/screens/IndexScreen.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { TransactionCard } from "../components/TransactionCard";
+import { View, StyleSheet } from "react-native";
 
 export const IndexScreen = () => {
-    return <TransactionCard />;
+    return <View style={style.container}>
+        <TransactionCard />
+    </View>;
 };
+
+const style = StyleSheet.create({
+    container: {
+        padding: 4,
+    },
+});
