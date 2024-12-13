6e309c8c80e10ffbab755614089889fbd5cd9125
Andra Antariksa <andra.antariksa@gmail.com>
2024-12-13 19:17:43 +0700

    feat: remove built-in navigation header

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
-import {
-    useQuery,
-    useMutation,
-    useQueryClient,
-    QueryClient,
-    QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MainNavigation from "./src/ui/routes";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 const queryClient = new QueryClient();
 
 export default function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <MainNavigation />
+            <SafeAreaView style={style.container}>
+                <MainNavigation />
+            </SafeAreaView>
         </QueryClientProvider>
     );
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
