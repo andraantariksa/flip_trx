0f3cb1a999b825a252060876626f37797b1bf5d5
Andra Antariksa <andra.antariksa@gmail.com>
2024-12-14 19:19:36 +0700

    feat: add data fetch feedback

--- src/ui/screens/index/components/Transactions.tsx
+++ src/ui/screens/index/components/Transactions.tsx
@@ -1,9 +1,18 @@
 import { memo } from "react";
-import { FlatList, ListRenderItem, StyleSheet, View } from "react-native";
+import {
+    FlatList,
+    ListRenderItem,
+    RefreshControl,
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+} from "react-native";
 import useTransactionsQuery from "../../../hooks/useTransactionsQuery";
 import { SortByKey } from "./FilterModal";
 import Transaction from "../../../../domain/entities/transaction";
 import { TransactionCard } from "./TransactionCard";
+import { Colors } from "../../../colors";
 
 const TransactionSeparator = () => <View style={style.separatorTransaction} />;
 
@@ -17,11 +26,31 @@ export type TransactionsProps = {
 };
 
 const Transactions = ({ sortBy, searchQuery }: TransactionsProps) => {
-    const { data } = useTransactionsQuery(sortBy, searchQuery);
+    const { data, isFetching, refetch, error } = useTransactionsQuery(
+        sortBy,
+        searchQuery,
+    );
+
+    if (error) {
+        return (
+            <View style={style.containerRetry}>
+                <Text style={style.textError}>{error.toString()}</Text>
+                <TouchableOpacity
+                    style={style.buttonRetry}
+                    onPress={() => refetch()}
+                >
+                    <Text style={style.textRetry}>Retry</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 
     return (
         <FlatList
             data={data ?? []}
+            refreshControl={
+                <RefreshControl refreshing={isFetching} onRefresh={refetch} />
+            }
             renderItem={TransactionItem}
             ItemSeparatorComponent={TransactionSeparator}
             contentContainerStyle={style.containerTransactionsContent}
@@ -41,5 +70,26 @@ const style = StyleSheet.create({
     },
     transactionsContent: {
         flex: 1,
+    },
+    textError: {
+        color: Colors.Orange,
+        fontSize: 16,
+        textAlign: "center",
+    },
+    containerRetry: {
+        marginTop: 16,
+        alignItems: "center",
+        gap: 8,
+    },
+    textRetry: {
+        fontSize: 12,
+        textAlign: "center",
+        color: Colors.White,
+    },
+    buttonRetry: {
+        backgroundColor: Colors.Orange,
+        paddingHorizontal: 20,
+        paddingVertical: 8,
+        borderRadius: 8,
     },
 });
