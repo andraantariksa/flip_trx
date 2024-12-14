041ca75d92234165c5180cc8d8272c1b70b2af8a
Andra Antariksa <andra.antariksa@gmail.com>
2024-12-14 11:32:32 +0700

    fix: navigation warn

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,9 @@
-export const formatDate = (date: Date) => {
-    const dayInMonth = date.toLocaleString("id-ID", { day: "numeric" });
-    const month = date.toLocaleString("id-ID", { month: "long" });
-    const year = date.toLocaleString("id-ID", { year: "numeric" });
+export const formatDate = (date: string) => {
+    const dateObj = new Date(date);
+
+    const dayInMonth = dateObj.toLocaleString("id-ID", { day: "numeric" });
+    const month = dateObj.toLocaleString("id-ID", { month: "long" });
+    const year = dateObj.toLocaleString("id-ID", { year: "numeric" });
 
     return `${dayInMonth} ${month} ${year}`;
 };
