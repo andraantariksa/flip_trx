a01230ef5929b80073f2c389bfa44fe827608931
Andra Antariksa <andra.antariksa@gmail.com>
2024-12-14 17:21:35 +0700

    fix: date sorter

--- src/ui/hooks/useTransactionsQuery.tsx
+++ src/ui/hooks/useTransactionsQuery.tsx
@@ -64,9 +64,9 @@ export default function useTransactionsQuery(
             (trxA: Transaction, trxB: Transaction) => number
         > = {
             dateAsc: (trxA, trxB) =>
-                trxA.createdAt.getTime() - trxB.createdAt.getTime(),
+                new Date(trxA.createdAt).getTime() - new Date(trxB.createdAt).getTime(),
             dateDesc: (trxA, trxB) =>
-                trxB.createdAt.getTime() - trxA.createdAt.getTime(),
+                new Date(trxB.createdAt).getTime() - new Date(trxA.createdAt).getTime(),
             nameAsc: (trxA, trxB) =>
                 trxA.receiverName.localeCompare(trxB.receiverName),
             nameDesc: (trxA, trxB) =>
